e340b5ea46ea54077b039eb0466b2bbf26b2191d
J Khalaf <jwan.khalaf1@nhs.net>
2020-02-12 11:53:43 +0000

    add build artifacts

---
+++ build/precache-manifest.7dc4680832c3816d8a8e78ff94801e65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d25eb4f5c2678cc93a6c9103d3be0fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50011c19ec6a1fa7992b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d2c5fe29b3b6de38a3e3",
+    "url": "/static/js/2.98caf6d1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.98caf6d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50011c19ec6a1fa7992b",
+    "url": "/static/js/main.29060c82.chunk.js"
+  },
+  {
+    "revision": "d165ca5f74e69d358f17",
+    "url": "/static/js/runtime-main.87ac21d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
